b5aaa012b1890e48a7b3109ea532060313131359
Thysmall <jordan@dbimages.com>
2026-03-11 15:29:42 -0400

    Die class creation

--- unit_9_participation.py
+++ unit_9_participation.py
@@ -4,3 +4,11 @@ Author: Jordan Betz
 Description: Class based dice rolling program
 Date: 3/11/2026
 """
+from random import randint
+
+class Die():
+    def __init__(self, faceCount: int):
+        self.faceCount = faceCount
+        
+    def roll_die(self):
+        return randint(1,self.faceCount)
