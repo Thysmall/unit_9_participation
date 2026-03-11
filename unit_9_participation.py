e32ba40ef43f6630ad2b4ed46fbdd078d9c67fea
Thysmall <jordan@dbimages.com>
2026-03-11 15:36:04 -0400

    Added temporary demo

--- unit_9_participation.py
+++ unit_9_participation.py
@@ -12,3 +12,9 @@ class Die():
         
     def roll_die(self):
         return randint(1,self.faceCount)
+    
+if __name__ == "__main__":
+    die_1: Die = Die(6)
+    print(f"You rolled a {die_1.roll_die()} on a {die_1.faceCount} sided die")
+    die_2: Die = Die(100)
+    print(f"You rolled a {die_2.roll_die()} on a {die_2.faceCount} sided die")
