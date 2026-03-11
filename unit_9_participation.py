e609b457f72f48ecec856d8f5348847ae5fb1a3f
Thysmall <jordan@dbimages.com>
2026-03-11 15:36:52 -0400

    Rename attribute to sides to follow textbook instructions

--- unit_9_participation.py
+++ unit_9_participation.py
@@ -7,14 +7,14 @@ Date: 3/11/2026
 from random import randint
 
 class Die():
-    def __init__(self, faceCount: int):
-        self.faceCount = faceCount
+    def __init__(self, sides: int):
+        self.sides = sides
         
     def roll_die(self):
-        return randint(1,self.faceCount)
+        return randint(1,self.sides)
     
 if __name__ == "__main__":
     die_1: Die = Die(6)
-    print(f"You rolled a {die_1.roll_die()} on a {die_1.faceCount} sided die")
+    print(f"You rolled a {die_1.roll_die()} on a {die_1.sides} sided die")
     die_2: Die = Die(100)
-    print(f"You rolled a {die_2.roll_die()} on a {die_2.faceCount} sided die")
+    print(f"You rolled a {die_2.roll_die()} on a {die_2.sides} sided die")
